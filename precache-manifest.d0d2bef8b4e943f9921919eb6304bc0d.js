237e70a94653f96b0440e83f22cfeb856c26d07b
atiwat02 <atiwat.ka.61@ubu.ac.th>
2020-09-30 10:03:32 +0700

    Updates

---
+++ precache-manifest.d0d2bef8b4e943f9921919eb6304bc0d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b790c1726d87f9ff49b383fcd69e8cf0",
+    "url": "/Projectcommath/index.html"
+  },
+  {
+    "revision": "c28e27cc23794f216137",
+    "url": "/Projectcommath/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19a1e6166864f58ff2f9",
+    "url": "/Projectcommath/static/js/2.a693db1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Projectcommath/static/js/2.a693db1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28e27cc23794f216137",
+    "url": "/Projectcommath/static/js/main.40a806c8.chunk.js"
+  },
+  {
+    "revision": "3d61fb834c565f38a9b4",
+    "url": "/Projectcommath/static/js/runtime-main.ca16024a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Projectcommath/static/media/logo.5d5d9eef.svg"
+  }
+]);
